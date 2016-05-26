e3b8ffa8efea663e7d92c2443ede0bd047691f8f
Stanislav Ovsyannikov <naphaso@gmail.com>
2016-05-26 19:58:56 +0600

    inital commit

---
+++ src/main/java/com/naphaso/testproject/Main.java
@@ -0,0 +1,10 @@
+package com.naphaso.testproject;
+
+/**
+ * Created by wolong on 26/05/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is a test project");
+    }
+}
